dbed67592d9d1ac49857527d843caa2b04e7aef7
Ben Gooch <bengooch7@gmail.com>
2022-07-28 13:21:25 -0400

    Changed name added to ansible hostname file

--- provision.sh
+++ provision.sh
@@ -45,7 +45,7 @@ main() {
     echo -e "Installing Ansible...\n"
     DEBIAN_FRONTEND=noninteractive sudo apt install -y ansible git
 
-    echo -e "${uname --nodename}" >> /etc/ansible/hosts
+    echo ${uname --nodename} >> /etc/ansible/hosts
 
 
     echo -e "Ready to go! Running Ansible playbooks now.\n\n\n"
