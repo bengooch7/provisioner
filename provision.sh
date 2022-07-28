a9722a794e53c2de98c489475ddf1f7a1267d383
Ben Gooch <bengooch7@gmail.com>
2022-07-28 11:24:33 -0400

    Added fancy text to start script

--- provision.sh
+++ provision.sh
@@ -1,22 +1,34 @@
 #!/usr/bin/env bash
 
 
-choice=""
+# For colorization
+RED='\033[0;31m'  # Red for error messages
+YLW='\033[0;33m' # Yellow for notes
+NO_COLOR='\033[0m'      # Revert terminal back to no color
 
-while [[ $choice != 'p' && $choice != 'w' ]]
-do
-    echo -e "Personal ('p') or Work ('w') environment? \n (p/w) > "
+welcome() {
+    echo -e "${YLW}Welcome to Ben's Provisioner!\n\n${NO_COLOR}"
 
-    read choice
 
-    if [[ $choice == 'p' ]]
-    then
-        echo "Provisioning for a personal device"
-        personal=1
-    else
-        personal=0
-    fi
-done
+    choice=""
+
+    while [[ $choice != 'p' && $choice != 'w' ]]
+    do
+        echo -e "Are you setting up a ${RED}Personal${NO_COLOR} ('${RED}p${NO_COLOR}') or ${YLW}Work${NO_COLOR} ('${YLW}w${NO_COLOR}') environment? \n (${RED}p${NO_COLOR}/${YLW}w${NO_COLOR}) > "
+
+        read choice
+
+        if [[ $choice == 'p' ]]
+        then
+            echo "Provisioning for a ${RED}personal${NO_COLOR} device"
+            personal=1
+        else
+            echo "Provisioning for a ${YLW}work${NO_COLOR} device"
+            personal=0
+        fi
+    done
+
+}
 
 main() {
     DEBIAN_FRONTEND=noninteractive sudo apt install -y software-properties-common
@@ -24,5 +36,7 @@ main() {
     DEBIAN_FRONTEND=noninteractive sudo apt install -y ansible git
     ansible-pull -U https://github.com/bengooch7/provisioner -e personal=$personal
 }
+
+welcome
 
 main
